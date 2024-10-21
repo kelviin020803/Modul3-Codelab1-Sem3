fdee09a88c3f09383cc3c88df8fb73baa80b68c2
kelviin020803 <kelvin020803@gmail.com>
2024-10-21 06:14:58 -0700

    Modul3Codelab1Sem3

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int x = 5; // nilai x
+        int y = 10; // nilai y
+        int sum = x + y; // hasil dari penjumlahan nilai x dan y
+        System.out.println("Sum: " + sum); // menampilkan hasil penjumlahan
+    }
+}
